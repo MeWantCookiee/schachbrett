fc2340ce93507718f03eab5568ef66a619b6aef5
MeeWantCookiee <49725401+MeeWantCookiee@users.noreply.github.com>
2024-02-17 02:41:47 +0100

    Button returns value of inputs to console

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useRef } from 'react';
 import './App.css';
 
-function App() {
+export default function Board() {
+  const xSizeRef = useRef(null); /* useRef hook is used here to create references to the input elements */ 
+  const ySizeRef = useRef(null);
+
+  function createGridArray() {
+    const gridArray = [];
+    const xSize = xSizeRef.current.value;
+    const ySize = ySizeRef.current.value;
+    
+    console.log(xSize, ySize);
+    
+    //create rows
+    for(let i = 0; i < xSize; i++) {
+      const row = [];
+      
+      //create columns
+      for(let j = 0; j < ySize; j++) {
+        //row.push(Square(i, j));
+        row.push();
+      }
+
+      gridArray.push(row);
+    }
+
+    return gridArray;
+  }
+  
+
+  //TODO: add error handling for x/y 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><div>
+      <label>
+        X:
+        <input ref={xSizeRef} type="number" />
+      </label>
+    </div><div>
+        <label>
+          Y:
+          <input ref={ySizeRef} type="number" />
+        </label>
+    </div><div>
+        <button type="button" onClick={createGridArray}> Erstellen </button>
+    </div></>
+    
   );
 }
-
-export default App;
